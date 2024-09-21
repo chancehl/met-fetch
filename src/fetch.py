3e6d5a815cc4448a77f164b5c146f91c052b4e45
Chance Linz <clinz.dev@gmail.com>
2024-09-20 17:36:30 -0700

    added report generation to cli

--- src/fetch.py
+++ src/fetch.py
@@ -5,23 +5,21 @@ from yaspin import yaspin
 from yaspin.spinners import Spinners
 from services.met import download_artwork, get_artwork, search_for_artwork
 from utils.list import pick_random_object
-from models.artwork import is_valid_artwork, print_artwork_name
+from models.artwork import is_valid_artwork, format_artwork_name, print_report
 from models.args import CommandLineArguments
 
 
 def main():
-    """This is the entrypoint of the application"""
-
     # this is needed in order to initialize the ANSI terminal colors on Windows, Linux
     os.system("")
+
     arguments = CommandLineArguments()
 
     count = arguments.get_count()
     outdir = arguments.get_outdir()
     query = arguments.get_query()
 
-    viewed = []
-    downloaded_pieces = 0
+    processed = []
 
     with yaspin(
         Spinners.dots, text="Searching for artwork...", color="cyan"
@@ -29,23 +27,23 @@ def main():
         # search for the query before looping
         matching_pieces = search_for_artwork(query)
 
-        while downloaded_pieces < count:
+        while len(processed) < count:
             random_id = pick_random_object(matching_pieces)
 
             artwork = get_artwork(object_id=random_id)
 
-            if is_valid_artwork(artwork, viewed):
-                # Update spinner
-                spinner.text = f"Downloading {print_artwork_name(artwork)}"
+            if is_valid_artwork(artwork, processed):
+                # update spinner
+                spinner.text = f"Downloading {format_artwork_name(artwork)}"
 
                 # download the file to specified location
                 download_artwork(artwork, outdir)
 
                 # save this so we don't re-download the same image
-                viewed.append(artwork["objectID"])
+                processed.append(artwork)
 
-                # increment count after successful download
-                downloaded_pieces += 1
+    # print report to console
+    print_report(processed, outdir)
 
     # exit process
     sys.exit(0)
--- src/models/artwork.py
+++ src/models/artwork.py
@@ -33,7 +33,7 @@ def get_artist_name(artwork: MuseumArtwork) -> str:
     return artists[0].get("name") if artists is not None else "Unknown artist"
 
 
-def print_artwork_name(artwork: MuseumArtwork):
+def format_artwork_name(artwork: MuseumArtwork):
     artist_name = get_artist_name(artwork)
     department = artwork.get("department")
     title = artwork.get("title")
@@ -42,11 +42,28 @@ def print_artwork_name(artwork: MuseumArtwork):
     return f'"{color(title, CYAN)}" by {color(artist_name, GREEN)} ({object_date}, {department} department)'
 
 
-def is_valid_artwork(artwork, viewed: List[int]) -> bool:
+def is_valid_artwork(artwork, processed: List[MuseumArtwork]) -> bool:
     artwork_id = artwork["objectID"]
     image_url = artwork["primaryImage"]
 
-    if image_url is not None and len(image_url) > 0 and artwork_id not in viewed:
+    if (
+        image_url is not None
+        and len(image_url) > 0
+        and not is_already_processed(artwork_id, processed)
+    ):
         return True
 
     return False
+
+
+def is_already_processed(artwork_id: int, processed: List[MuseumArtwork]) -> bool:
+    return any(a.get("objectID") == artwork_id for a in processed)
+
+
+def print_report(artwork: List[MuseumArtwork], outdir: str) -> None:
+    print("Downloaded the following pieces:\n")
+
+    for art in artwork:
+        print(f"* {format_artwork_name(art)}")
+
+    print(f"\nSaved artwork to: {outdir}")
